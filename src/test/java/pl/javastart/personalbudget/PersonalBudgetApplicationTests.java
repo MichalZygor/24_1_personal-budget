95a41bc6e80a14ea6897e72dcac174fe0b2117bb
Michał <michal@winami.com>
2021-01-29 20:57:12 +0100

    clean environment

---
+++ src/test/java/pl/javastart/personalbudget/PersonalBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.javastart.personalbudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalBudgetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
